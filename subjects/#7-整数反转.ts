3e1155b28f14b8d970c0c67aea6a35ff32b942a1
caoxicheng <dove_justdoit@outlook.com>
2020-10-27 08:09:53 +0800

    更加艺术

--- subjects/#7-整数反转.ts
+++ subjects/#7-整数反转.ts
@@ -1,20 +1,12 @@
 function reverse(x: number): number {
-  let symbol: boolean = x >= 0 ? true : false;
+  const range: number = Math.pow(2,31);
 
-  let tempArray: Array<string> = x.toString().split('');
-  if (!symbol) {
-    tempArray.splice(0, 1);
-  }
-  tempArray.reverse();
-  let tempString: string = tempArray.join('');
-  let tempInt: number = parseInt(tempString);
-  if (!symbol) {
-    tempInt = tempInt * -1;
+  let y: number = 0;
+  while (x) {
+    let k = x % 10;
+    y = y * 10 + k;
+    x = (x - k) / 10;
   }
 
-  if (tempInt > (Math.pow(2, 31) - 1) || tempInt < -Math.pow(2, 31)) {
-    tempInt = 0;
-  }
-
-  return tempInt;
+  return (y >= range || y < -range) ? 0 : y;
 };
