721572142757e2e81d301578b08bf60b45f4f4c5
caoxicheng <dove_justdoit@outlook.com>
2020-10-28 18:12:55 +0800

    这才是艺术！

--- subjects/#13-罗马数字转整数.ts
+++ subjects/#13-罗马数字转整数.ts
@@ -1,40 +1,18 @@
 function romanToInt(s: string): number {
-  let result: number = 0;
-  let tempArr: Array<string> = [...s];
-  tempArr.forEach((x, index) => {
-    let symbol: number = 1;
-    switch (x) {
-      case "I":
-        if (tempArr[index + 1] === 'V' || tempArr[index + 1] === 'X') {
-          symbol = -symbol;
-        }
-        result += symbol*1;
-        break;
-      case "V":
-        result += 5;
-        break;
-      case "X":
-        if (tempArr[index + 1] === 'L' || tempArr[index + 1] === 'C') {
-          symbol = -symbol;
-        }
-        result += symbol*10;
-        break;
-      case "L":
-        result += 50;
-        break;
-      case "C":
-        if (tempArr[index + 1] === 'D' || tempArr[index + 1] === 'M') {
-          symbol = -symbol;
-        }
-        result += symbol*100;
-        break;
-      case "D":
-        result += 500;
-        break;
-      case "M":
-        result += 1000;
-        break;
-    }
-  });
-  return result;
+  let num: number = 0;
+  const ROMA_MAP = {
+    M: 1000,
+    D: 500,
+    C: 100,
+    L: 50,
+    X: 10,
+    V: 5,
+    I: 1,
+  } as any;
+  for (let i = 0; i < s.length; i++) {
+    let cur = ROMA_MAP[s[i]];
+    let next = ROMA_MAP[s[i + 1]] || 0;
+    cur < next ? (num -= cur) : (num += cur);
+  }
+  return num;
 }
