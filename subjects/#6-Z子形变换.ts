887b3d6db850acdddd3f016aa463f9be1ddef5ee
caoxicheng <dove_justdoit@outlook.com>
2020-11-30 18:29:14 +0800

    初次保存

---
+++ subjects/#6-Z子形变换.ts
@@ -0,0 +1,32 @@
+function convert(s: string, numRows: number): string {
+  if (numRows === 1) {
+    return s;
+  }
+  let zigzag: Array<any> = [];
+  
+  for(let index = 0; index < numRows; index++) {
+    zigzag.push([]);
+  }
+
+  let row: number = 0, col: number = 0;
+  for (let index = 0; index < s.length; index++) {
+    let element = s[index];
+    if ( col % (numRows - 1) === 0) {
+      zigzag[row++][col] = element;
+      if (row === numRows) {
+        col++;
+      }
+    } else {
+      row = (numRows - 1) - (col & (numRows - 1));
+      zigzag[row][col++] = element;
+      console.log(element, row, col);
+      row = 0;
+    }
+  }
+  
+  let result = '';
+  zigzag.forEach( row => {
+    result += row.join('')
+  })
+  return result
+};
