09bebb4bdf04742440b88926d42ee6bc101eeac1
caoxicheng <dove_justdoit@outlook.com>
2020-12-14 14:01:41 +0800

    双指针

--- subjects/#28-实现strStr.ts
+++ subjects/#28-实现strStr.ts
@@ -2,10 +2,27 @@ function strStr(haystack: string, needle: string): number {
   let L: number = needle.length;
   let n: number = haystack.length;
 
-  for(let start = 0; start < n - L + 1; start++) {
-    if (haystack.slice(start, start + L) === needle) {
-      return start;
+  if (L === 0) {
+    return 0;
+  }
+
+  let pn: number = 0;
+
+  while(pn + L < n + 1) {
+    while(pn + L < n + 1 && needle[0] !== haystack[pn]) pn++;
+
+    let currLen = 0, pl = 0;
+    while (pl < L && pn < n && needle[pl] === haystack[pn]) {
+      currLen++;
+      pl++;
+      pn++;
     }
+
+    if (currLen === L) {
+      return pn - L;
+    }
+
+    pn = pn - currLen + 1;
   }
 
   return -1;
