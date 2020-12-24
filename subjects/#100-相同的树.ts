b7f3865f686ac3655dd92de2a1f1ca34662440de
caoxicheng <dove_justdoit@outlook.com>
2020-12-24 21:12:23 +0800

    递归

---
+++ subjects/#100-相同的树.ts
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+  let left: number[] = node(p), right: number[] = node(q);
+  if (left.length !== right.length) {
+    return false;
+  }
+
+  for (let i = 0; i < left.length; i++) {
+    if (left[i] !== right[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function node(N: TreeNode | null): number[] {
+  if (N === null) {
+    return [null];
+  }
+
+  if (N.left === null && N.right === null) {
+    return [N.val];
+  }
+
+  return [N.val].concat(node(N.left), node(N.right));
+}
