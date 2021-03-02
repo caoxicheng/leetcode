c2af92380979eeaa55499699faf54720d0d8e9c7
caoxicheng <dove_justdoit@outlook.com>
2021-03-02 21:00:39 +0800

    轻车熟路

---
+++ subjects/#111-二叉树的最小深度.ts
@@ -0,0 +1,24 @@
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function minDepth(root: TreeNode | null): number {
+  if (root === null) {
+    return 0;
+  } else if (root.left === null && root.right === null) {
+    return 1;
+  } else if (root.left === null && root.right !== null) {
+    return minDepth(root.right) + 1;
+  } else if (root.left !== null && root.right === null) {
+    return minDepth(root.left) + 1;
+  } else {
+    return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+  }
+}
