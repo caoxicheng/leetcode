6a413e28513493e994892d2f078fae61db49778c
caoxicheng <dove_justdoit@outlook.com>
2021-03-18 17:05:44 +0800

    穿针引线

--- subjects/#92-反转链表II.ts
+++ subjects/#92-反转链表II.ts
@@ -8,44 +8,18 @@ class ListNode {
 }
 
 function reverseBetween(head: ListNode | null, left: number, right: number): ListNode | null {
-  let result = head;
-  let temp: Array<ListNode | null> = [];
-  let index: number = 1;
-  let beforeItem: ListNode;
-  let afterItem: ListNode;
-  while (head !== null) {
-    // 如果有前一项
-    if (index === left - 1) {
-      beforeItem = head;
-    }
-    // 如果有后一项
-    if (index === right + 1) {
-      afterItem = head;
-    }
-    if (index >= left && index <= right) {
-      temp.unshift(head);
-    }
-    index++;
-    head = head.next;
+  const dummy_node = new ListNode(-1);
+  dummy_node.next = head;
+  let pre = dummy_node;
+  for (let i = 0; i < left - 1; ++i) {
+    pre = pre.next;
   }
-  if (temp.length > 1) {
-    let tempList = temp[0];
-    let tempHead = temp[0];
-    for (let i = 1 , l = temp.length; i < l ; ++i) {
-      tempList.next = temp[i];
-      tempList = tempList.next;
-    }
-    // 如果有后一项，则接入链表
-    if (afterItem !== void 0) {
-      tempList.next = afterItem;
-    } else {
-      tempList.next = null;
-    }
-    // 如果无前一线，则直接返回临时链表
-    if (beforeItem === void 0) {
-      return tempHead;
-    }
-    beforeItem.next = tempHead;
+  let cur = pre.next;
+  for (let i = 0; i < right - left; ++i) {
+    const next = cur.next;
+    cur.next = next.next;
+    next.next = pre.next;
+    pre.next = next;
   }
-  return result;
+  return dummy_node.next;
 }
