141dbe9f20c8ef387fa64e1272f0020647b9a43d
caoxicheng <dove_justdoit@outlook.com>
2021-03-22 22:16:22 +0800

    无符号右移，配合短路运算

--- subjects/#191-位1的位数.ts
+++ subjects/#191-位1的位数.ts
@@ -1,10 +1,8 @@
 function hammingWeight(n: number): number {
-  let binaryNum: string = n.toString(2);
-  let numList: Array<string> = binaryNum.split('');
-  return numList.reduce( (accumulator, currentValue) => {
-    if (currentValue == '1') {
-      accumulator += 1;
-    }
-    return accumulator;
-  }, 0);
+  let result = 0;
+  while (n > 0) {
+    n % 2  && (++result);
+    n >>>= 1;
+  }
+  return result;
 };
