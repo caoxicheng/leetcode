cd712dcd1f7a5a6c36974478dcf52cbe8615f561
caoxicheng <dove_justdoit@outlook.com>
2021-03-24 17:57:21 +0800

    双指针

--- subjects/#167- 两数之和II-输入有序数组.ts
+++ subjects/#167- 两数之和II-输入有序数组.ts
@@ -1,11 +1,15 @@
 function twoSum(numbers: number[], target: number): number[] {
-  let end:number = numbers.length;
+  let pointerLeft: number = 0, pointerRight: number = numbers.length - 1;
+  let result: number;
 
-  while (end >=0) {
-    let start = numbers.slice(0, end).indexOf((target - numbers[end]));
-    if (start !== -1) {
-      return [start + 1, end + 1];
+  while (pointerLeft < pointerRight) {
+    result = numbers[pointerLeft] + numbers[pointerRight];
+    if (result > target) {
+      pointerRight--;
+    } else if (result < target) {
+      pointerLeft++;
+    } else {
+      return [pointerLeft + 1, pointerRight + 1];
     }
-    --end;
   }
 };
