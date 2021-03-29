c62f020a972d0a5bab18b1cd1068220a65418c9f
caoxicheng <dove_justdoit@outlook.com>
2021-03-29 15:29:51 +0800

    排序

--- subjects/#169-多数元素.ts
+++ subjects/#169-多数元素.ts
@@ -1,17 +1,3 @@
 function majorityElement(nums: number[]): number {
-  let len = nums.length;
-  if (len === 1) {
-    return nums[0];
-  }
-  let majorityNumber: number = Math.floor(len / 2);
-  const stack = Object.create(null);
-  for (let i = 0; i < len; ++i) {
-    if (stack[nums[i]] !== void 0) {
-      if (++stack[nums[i]] > majorityNumber) {
-        return nums[i];
-      }
-    } else {
-      stack[nums[i]] = 1;
-    }
-  }
+  return nums.sort()[Math.floor(nums.length/2)];
 };
