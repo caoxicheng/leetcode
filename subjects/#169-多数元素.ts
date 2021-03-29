447ea192475156ea6e64e03e6fc44c5470984652
caoxicheng <dove_justdoit@outlook.com>
2021-03-29 16:12:59 +0800

    候选人算法

--- subjects/#169-多数元素.ts
+++ subjects/#169-多数元素.ts
@@ -1,3 +1,13 @@
 function majorityElement(nums: number[]): number {
-  return nums.sort()[Math.floor(nums.length/2)];
+  let count: number = 0;
+  let candidate: number = undefined;
+
+  for (let i = 0, len = nums.length; i < len; ++i) {
+    if (count === 0) {
+      candidate = nums[i];
+    }
+
+    count += (nums[i] === candidate) ? 1 : -1;
+  }
+  return candidate;
 };
