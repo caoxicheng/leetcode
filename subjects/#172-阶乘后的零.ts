03261ce8b972580026f1d4a4a19fb8ccb98b6268
caoxicheng <dove_justdoit@outlook.com>
2021-03-29 17:59:53 +0800

    按位或

--- subjects/#172-阶乘后的零.ts
+++ subjects/#172-阶乘后的零.ts
@@ -1,8 +1,9 @@
 function trailingZeroes(n: number): number {
-  let sum = 0, n5 = 5;
-  while ((n / n5) >= 1) {
-    sum += Math.floor(n / n5);
-    n5 *= 5;
+  let sum = 0;
+  while ( n >= 5) {
+    sum += n/5 | 0;
+    // 按位或，等同于 向下取整，因为会转换成整数，去掉小数部分
+    n /= 5;
   }
   return sum;
 };
